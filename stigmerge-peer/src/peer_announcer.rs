d8ed2c773e58f893bb2f84b9399109029b089703
Casey Marshall <git@cmars.tech>
2025-04-17 23:20:26 -0500

    test: coverage for peer_announcer
    Fixed a bug where peer_indexes weren't being tracked, and could not be
    redacted.

--- stigmerge-peer/src/peer_announcer.rs
+++ stigmerge-peer/src/peer_announcer.rs
@@ -44,7 +44,9 @@ impl<P: Peer> Service for PeerAnnouncer<P> {
     type Response = Response;
 
     async fn run(mut self, cancel: CancellationToken) -> Result<()> {
-        self.peer.reset_peers(self.key.to_owned(), self.max_peers).await?;
+        self.peer
+            .reset_peers(self.key.to_owned(), self.max_peers)
+            .await?;
         loop {
             select! {
                 _ = cancel.cancelled() => {
@@ -115,10 +117,250 @@ impl<P: Peer> PeerAnnouncer<P> {
         for (i, maybe_key) in self.peers.iter_mut().enumerate() {
             if let None = maybe_key {
                 *maybe_key = Some(key);
+
+                self.peer_indexes.insert(key, i);
                 return i.try_into().unwrap();
             }
         }
         self.peers.push(Some(key));
-        (self.peers.len() - 1).try_into().unwrap()
+        let i = self.peers.len() - 1;
+        self.peer_indexes.insert(key, i);
+        i.try_into().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        str::FromStr,
+        sync::{Arc, Mutex, RwLock},
+    };
+
+    use tokio_util::sync::CancellationToken;
+    use veilid_core::TypedKey;
+
+    use crate::{
+        chan_rpc::{pipe, Service},
+        error::Result,
+        peer_announcer::{PeerAnnouncer, Request},
+        tests::StubPeer,
+    };
+
+    #[tokio::test]
+    async fn test_peer_announcer_announces_peer() -> Result<()> {
+        // Create a stub peer with a recording announce_peer_result
+        let mut stub_peer = StubPeer::new();
+        let recorded_key = Arc::new(RwLock::new(None));
+        let recorded_peer_key = Arc::new(RwLock::new(None));
+        let recorded_index = Arc::new(RwLock::new(None));
+
+        let recorded_key_clone = recorded_key.clone();
+        let recorded_peer_key_clone = recorded_peer_key.clone();
+        let recorded_index_clone = recorded_index.clone();
+
+        let recorded_resets = Arc::new(RwLock::new(0u32));
+        let recorded_resets_clone = recorded_resets.clone();
+        stub_peer.reset_peers_result = Arc::new(Mutex::new(move |_key, _subkeys| {
+            let mut count = recorded_resets_clone.write().unwrap();
+            *count += 1;
+            Ok(())
+        }));
+
+        stub_peer.announce_peer_result = Arc::new(Mutex::new(
+            move |key: TypedKey, peer_key: Option<TypedKey>, index: u16| {
+                *recorded_key_clone.write().unwrap() = Some(key);
+                *recorded_peer_key_clone.write().unwrap() = peer_key;
+                *recorded_index_clone.write().unwrap() = Some(index);
+                Ok(())
+            },
+        ));
+
+        // Create a test key and channel
+        let test_key =
+            TypedKey::from_str("VLD0:cCHB85pEaV4bvRfywxnd2fRNBScR64UaJC8hoKzyr3M").expect("key");
+        let peer_key =
+            TypedKey::from_str("VLD0:dDHB85pEaV4bvRfywxnd2fRNBScR64UaJC8hoKzyr3M").expect("key");
+        let (mut client_ch, server_ch) = pipe(16);
+
+        // Create peer announcer
+        let peer_announcer = PeerAnnouncer::new(stub_peer.clone(), test_key.clone(), server_ch);
+
+        // Create cancellation token
+        let cancel = CancellationToken::new();
+        let cancel_task = cancel.clone();
+
+        // Spawn the peer announcer service
+        let handle = tokio::spawn(async move { peer_announcer.run(cancel_task).await });
+
+        // Send an Announce request
+        let req = Request::Announce {
+            key: peer_key.clone(),
+        };
+        client_ch.tx.send(req).await.unwrap();
+        client_ch
+            .rx
+            .recv()
+            .await
+            .expect("recv")
+            .expect("announce ok");
+
+        // Verify the peer was announced correctly
+        let recorded_key = recorded_key.read().unwrap();
+        let recorded_peer_key = recorded_peer_key.read().unwrap();
+        let recorded_index = recorded_index.read().unwrap();
+
+        assert!(recorded_key.is_some(), "Key was not recorded");
+        assert!(recorded_peer_key.is_some(), "Peer key was not recorded");
+        assert!(recorded_index.is_some(), "Index was not recorded");
+        assert_eq!(recorded_key.as_ref(), Some(&test_key));
+        assert_eq!(recorded_peer_key.as_ref(), Some(&peer_key));
+        assert_eq!(recorded_index.as_ref(), Some(&0));
+
+        assert_eq!(*recorded_resets.read().unwrap(), 1u32);
+
+        // Clean up
+        cancel.cancel();
+        handle.await.unwrap().unwrap();
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_peer_announcer_redacts_peer() -> Result<()> {
+        // Create a stub peer with recording announce_peer_result
+        let mut stub_peer = StubPeer::new();
+        let recorded_key = Arc::new(RwLock::new(None));
+        let recorded_peer_key = Arc::new(RwLock::new(None));
+        let recorded_index = Arc::new(RwLock::new(None));
+
+        let recorded_key_clone = recorded_key.clone();
+        let recorded_peer_key_clone = recorded_peer_key.clone();
+        let recorded_index_clone = recorded_index.clone();
+
+        let recorded_resets = Arc::new(RwLock::new(0u32));
+        let recorded_resets_clone = recorded_resets.clone();
+        stub_peer.reset_peers_result = Arc::new(Mutex::new(move |_key, _subkeys| {
+            let mut count = recorded_resets_clone.write().unwrap();
+            *count += 1;
+            Ok(())
+        }));
+
+        stub_peer.announce_peer_result = Arc::new(Mutex::new(
+            move |key: TypedKey, peer_key: Option<TypedKey>, index: u16| {
+                *recorded_key_clone.write().unwrap() = Some(key);
+                *recorded_peer_key_clone.write().unwrap() = peer_key;
+                *recorded_index_clone.write().unwrap() = Some(index);
+                Ok(())
+            },
+        ));
+
+        // Create a test key and channel
+        let test_key =
+            TypedKey::from_str("VLD0:cCHB85pEaV4bvRfywxnd2fRNBScR64UaJC8hoKzyr3M").expect("key");
+        let peer_key =
+            TypedKey::from_str("VLD0:dDHB85pEaV4bvRfywxnd2fRNBScR64UaJC8hoKzyr3M").expect("key");
+        let (mut client_ch, server_ch) = pipe(16);
+
+        // Create peer announcer
+        let peer_announcer = PeerAnnouncer::new(stub_peer.clone(), test_key.clone(), server_ch);
+
+        // Create cancellation token
+        let cancel = CancellationToken::new();
+        let cancel_task = cancel.clone();
+
+        // Spawn the peer announcer service
+        let handle = tokio::spawn(async move { peer_announcer.run(cancel_task).await });
+
+        // First announce a peer
+        let req_announce = Request::Announce {
+            key: peer_key.clone(),
+        };
+        client_ch.tx.send(req_announce).await.unwrap();
+        client_ch
+            .rx
+            .recv()
+            .await
+            .expect("recv")
+            .expect("announce ok");
+
+        // Then redact it
+        let req_redact = Request::Redact {
+            key: peer_key.clone(),
+        };
+        client_ch.tx.send(req_redact).await.unwrap();
+        client_ch.rx.recv().await.expect("recv").expect("redact ok");
+
+        // Verify the peer was redacted correctly
+        let recorded_key = recorded_key.read().unwrap();
+        let recorded_peer_key = recorded_peer_key.read().unwrap();
+        let recorded_index = recorded_index.read().unwrap();
+
+        assert!(recorded_key.is_some(), "Key was not recorded");
+        assert!(recorded_peer_key.is_none(), "Peer key was not recorded");
+        assert!(recorded_index.is_some(), "Index was not recorded");
+        assert_eq!(recorded_key.as_ref(), Some(&test_key));
+        assert_eq!(recorded_peer_key.as_ref(), None); // Redacted peer has None
+        assert_eq!(recorded_index.as_ref(), Some(&0));
+
+        assert_eq!(*recorded_resets.read().unwrap(), 1u32);
+
+        // Clean up
+        cancel.cancel();
+        handle.await.unwrap().unwrap();
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_peer_announcer_resets_peers() -> Result<()> {
+        // Create a stub peer with recording reset_peers_result
+        let mut stub_peer = StubPeer::new();
+        let recorded_key = Arc::new(RwLock::new(None));
+        let recorded_max_peers = Arc::new(RwLock::new(None));
+
+        let recorded_key_clone = recorded_key.clone();
+        let recorded_max_peers_clone = recorded_max_peers.clone();
+
+        stub_peer.reset_peers_result =
+            Arc::new(Mutex::new(move |key: TypedKey, max_peers: u16| {
+                *recorded_key_clone.write().unwrap() = Some(key);
+                *recorded_max_peers_clone.write().unwrap() = Some(max_peers);
+                Ok(())
+            }));
+
+        // Create a test key and channel
+        let test_key =
+            TypedKey::from_str("VLD0:cCHB85pEaV4bvRfywxnd2fRNBScR64UaJC8hoKzyr3M").expect("key");
+        let (mut client_ch, server_ch) = pipe(16);
+
+        // Create peer announcer
+        let peer_announcer = PeerAnnouncer::new(stub_peer.clone(), test_key.clone(), server_ch);
+
+        // Create cancellation token
+        let cancel = CancellationToken::new();
+        let cancel_task = cancel.clone();
+
+        // Spawn the peer announcer service
+        let handle = tokio::spawn(async move { peer_announcer.run(cancel_task).await });
+
+        // Send a Reset request
+        let req = Request::Reset;
+        client_ch.tx.send(req).await.unwrap();
+        client_ch.rx.recv().await.expect("recv").expect("reset ok");
+
+        // Verify the peers were reset correctly
+        let recorded_key = recorded_key.read().unwrap();
+        let recorded_max_peers = recorded_max_peers.read().unwrap();
+
+        assert!(recorded_key.is_some(), "Key was not recorded");
+        assert!(recorded_max_peers.is_some(), "Max peers was not recorded");
+        assert_eq!(recorded_key.as_ref(), Some(&test_key));
+        assert_eq!(recorded_max_peers.as_ref(), Some(&32)); // DEFAULT_MAX_PEERS
+
+        // Clean up
+        cancel.cancel();
+        handle.await.unwrap().unwrap();
+
+        Ok(())
     }
 }
